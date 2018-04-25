b108d0e1a93e6dab78294664206338eac0d35e91
GeorgeCrisan <georgerdp@gmail.com>
2018-04-25 14:00:25 +0100

    changebyun

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname + '/client/build/')));
 
 app.get('/',(req,res)=>{
     
-    res.sendFile(path.join(__dirname + `/app/build/index.html`));
+    res.sendFile(path.join(__dirname + `../app/build/index.html`));
 
 });
 
