7e0ee14e4700799fd3262bd4967401b1b370d38b
GeorgeCrisan <georgerdp@gmail.com>
2018-04-25 13:39:22 +0100

    cool

---
+++ server.js
@@ -0,0 +1,29 @@
+/*require npm modules----  */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const seesion = require('express-session');
+const path = require('path');
+const cors = require('cors');
+let port = 3200;
+/* set up server */
+const app = express();
+const configDBase = require('./my_modules/database.js');
+
+/*database code lines , later to move in they own module comonjs */
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname + '/client/build/')));
+
+app.get('/',(req,res)=>{
+    
+    res.sendFile(path.join(__dirname + `/client/build/index.html`));
+
+});
+
+app.listen(port || porcess.env.PORT,()=> console.log(`runing at ${port || process.env.PORT}`) );
