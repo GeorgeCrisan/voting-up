5aaa3ca3a2d45b5c8804ebf86037b4f2375682b8
GeorgeCrisan <georgerdp@gmail.com>
2018-05-21 22:39:03 +0100

    mergi desktop

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import MyApp from './App.js';
 import WebFont from 'webfontloader';
 import {BrowserRouter as Router } from 'react-router-dom';
-//gata de syncron
+//gata de syncron mergi destkop
 
 
 import registerServiceWorker from './registerServiceWorker';
