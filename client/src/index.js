8f9f8444680b7b7e45733f5b0325eca05e02c634
GeorgeCrisan <georgerdp@gmail.com>
2018-05-21 23:18:37 +0100

    final vs is king

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import MyApp from './App.js';
 import WebFont from 'webfontloader';
 import {BrowserRouter as Router } from 'react-router-dom';
-//deja e enervant 
+
 
 import registerServiceWorker from './registerServiceWorker';
 
