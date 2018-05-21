8f999afa289b80482d10cd022254942ce2fe8b0b
GeorgeCrisan <georgerdp@gmail.com>
2018-05-21 22:55:00 +0100

    ultimu send pe surf v1

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import MyApp from './App.js';
 import WebFont from 'webfontloader';
 import {BrowserRouter as Router } from 'react-router-dom';
-//de la inceput ..dute deskop
+//de la inceput ..dute deskop ... du-te te rog surf napoi
 
 
 import registerServiceWorker from './registerServiceWorker';
