e7cced0d53a5c284504ba91aadf89831233d4243
GeorgeCrisan <georgerdp@gmail.com>
2018-08-09 08:38:38 +0100

    run

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/',(req,res)=>{
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
